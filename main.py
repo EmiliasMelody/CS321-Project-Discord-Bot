b061dafce21f3affdcb7069213f4d80a908754dc
nikita <nsade.21@gmail.com>
2020-11-16 15:27:40 -0500

    commented out run command

--- main.py
+++ main.py
@@ -1298,4 +1298,4 @@ async def throwwaterballoon(ctx, user):
     await ctx.send(file=discord.File('waterballoon.gif'))
 
 
-client.run('NzY0MTgwMzU1MzU0ODUzNDE2.X4Cgag.cHZlrlH6jsYS7nkjVgBVnshM2JU')
+#client.run('NzY0MTgwMzU1MzU0ODUzNDE2.X4Cgag.cHZlrlH6jsYS7nkjVgBVnshM2JU')
